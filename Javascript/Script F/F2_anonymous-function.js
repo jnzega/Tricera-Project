12721d606e3d50f8eda34bc04028f6367bb33a4c
Joshua Zega <hedywidja321@gmail.com>
2024-04-16 23:22:59 +0700

    Continuing F materials about function (anonymous function)

---
+++ Javascript/Script F/F2_anonymous-function.js
@@ -0,0 +1,24 @@
+/*
+    Pada file sebelumnya sudah dipelajari bagaimana cara membuat
+    sebuah function. Di javascript kita dapat membuat function
+    tanpa menggunakan atau mendeklarasikan function tersebut 
+    di awal. Ini disebut anonymous function
+*/
+
+// contoh anonymous function yang disimpan pada sebuah variabel 
+let sambut = function (name) {
+    console.info(`Hello ${name}`)
+}
+
+sambut("Hehe") // output: Hello Joshua
+
+// anonymous function juga dapat diletakan dalam sebuah
+// parameter
+
+function giveMeName(fungsi) {
+    fungsi("Joshua")
+}
+
+giveMeName(function (name) {
+    console.log(`anjay ${name}`)
+})
